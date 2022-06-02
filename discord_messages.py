62cf7c195432e656ace06aeea8f3ee99e770a53f
Yuriy Kurochkin <Yuriy.Kurochkin@gmail.com>
2022-06-02 03:03:59 +0300

    add random tips

--- discord_messages.py
+++ discord_messages.py
@@ -1,4 +1,41 @@
 import discord
+import random
+
+
+class RandomTipper:
+    def __init__(self):
+        self.tips = list()
+        self.tips.append({'text': 'TARS tip: I understand my progress will be erased',
+                          'weight': 3})
+        self.tips.append({'text': 'TARS tip: Here we can wait for Dark Nebula release',
+                          'weight': 1})
+        self.tips.append({'text': 'TARS tip: ?out command returns you from queue to real world',
+                          'weight': 1})
+        self.tips.append({'text': 'TARS tip: ?start <level> <spec> command send even incomplete team to RS',
+                          'weight': 1})
+        self.last_seen = [2] * len(self.tips)
+        self.stats = [0] * len(self.tips)
+
+    def get_next_text(self):
+        s = list()
+        s.append(1000.0 * (1 - 1.0 / self.last_seen[0]) * self.tips[0]['weight'])
+        for i in range(1, len(self.tips)):
+            s.append(s[-1] + 1000.0 * (1 - 1.0 / self.last_seen[i]) * self.tips[i]['weight'])
+        th = random.randrange(int(s[-1]))
+        chosen = 0
+        for i in range(len(self.tips)):
+            if th <= s[i]:
+                chosen = i
+                break
+        for i in range(len(self.tips)):
+            self.last_seen[i] += 1
+        self.last_seen[chosen] = 1
+        self.stats[chosen] += 1
+        print(f'random stats {self.stats} last seen {self.last_seen}')
+        return self.tips[chosen]['text']
+
+
+tipper = RandomTipper()
 
 
 class TextMessage:
@@ -15,6 +52,7 @@ class StartRSMessage:
         color = discord.Colour.dark_red() if spec == "dark" else discord.Colour.red()
         self.embed = discord.Embed(title=title, type="rich", colour=color)
         self.embed.add_field(name='Team:', value='\n'.join(names), inline=False)
+        self.embed.set_footer(text=tipper.get_next_text())
 
     async def send(self, ctx):
         await ctx.send(embed=self.embed)
@@ -33,4 +71,3 @@ class QueueRSMessage:
 
     async def send(self, ctx):
         await ctx.send(embed=self.embed)
-
