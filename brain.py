ccfa2774a9caa33cbd44da042dba187bc9ace071
Yuriy Kurochkin <Yuriy.Kurochkin@gmail.com>
2022-06-09 00:47:36 +0300

    add locks

--- brain.py
+++ brain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+import asyncio
 
 from discord_messages import TextMessage
 from discord_messages import StartRSMessage
@@ -16,6 +17,7 @@ class Brain:
             self.queue[key] = list(value)
         self.user_storage = user_storage
         self.db = db
+        self.lock = asyncio.Lock()
 
     @staticmethod
     def build_key(level, spec):
@@ -68,51 +70,56 @@ class Brain:
             yield TextMessage('Empty queue')
 
     def status_command(self):
-        yield from self.generate_queue_text()
+        with self.lock:
+            yield from self.generate_queue_text()
 
     def in_command(self, user, level, spec):
-        key = self.build_key(level, spec)
-        if user.id not in self.queue[key]:
-            self.queue[key].append(user.id)
-            yield TextMessage(f'{user.display_name} enters queue for Red Star {level} {spec}')
-            lq = len(self.queue[key])
-            if lq == 4 or lq == 2 and (level == 2 or spec == 'duo' or spec == 'dark'):
+        with self.lock:
+            key = self.build_key(level, spec)
+            if user.id not in self.queue[key]:
+                self.queue[key].append(user.id)
+                yield TextMessage(f'{user.display_name} enters queue for Red Star {level} {spec}')
+                lq = len(self.queue[key])
+                if lq == 4 or lq == 2 and (level == 2 or spec == 'duo' or spec == 'dark'):
+                    yield from self.generate_start_text(level, spec)
+                    for queue_uid in list(self.queue[key]):
+                        self.clear_user_from_queue(queue_uid)
+                self.db.set_collection('queue', self.queue)
+                self.db.save()
+                yield from self.generate_queue_text()
+            else:
+                yield TextMessage(f'{user.display_name} already in queue for Red Star {level} {spec}')
+
+    def out_level_command(self, user, level, spec):
+        with self.lock:
+            key = self.build_key(level, spec)
+            if user.id not in self.queue[key]:
+                yield TextMessage(f'{user.display_name} not in queue for Red Star {level} {spec}')
+            else:
+                self.queue[key].remove(user.id)
+                self.db.set_collection('queue', self.queue)
+                self.db.save()
+                yield TextMessage(f'{user.display_name} leaves queue for Red Star {level} {spec}')
+                yield from self.generate_queue_text()
+
+    def start_command(self, user, level, spec):
+        with self.lock:
+            key = self.build_key(level, spec)
+            if user.id not in self.queue[key]:
+                yield TextMessage(f'{user.display_name} not in queue for Red Star {level} {spec}')
+            else:
+                yield TextMessage(f'{user.display_name} starts queue for Red Star {level} {spec}')
                 yield from self.generate_start_text(level, spec)
                 for queue_uid in list(self.queue[key]):
                     self.clear_user_from_queue(queue_uid)
-            self.db.set_collection('queue', self.queue)
-            self.db.save()
-            yield from self.generate_queue_text()
-        else:
-            yield TextMessage(f'{user.display_name} already in queue for Red Star {level} {spec}')
+                self.db.set_collection('queue', self.queue)
+                self.db.save()
+                yield from self.generate_queue_text()
 
-    def out_command_level(self, user, level, spec):
-        key = self.build_key(level, spec)
-        if user.id not in self.queue[key]:
-            yield TextMessage(f'{user.display_name} not in queue for Red Star {level} {spec}')
-        else:
-            self.queue[key].remove(user.id)
-            self.db.set_collection('queue', self.queue)
-            self.db.save()
-            yield TextMessage(f'{user.display_name} leaves queue for Red Star {level} {spec}')
+    def out_all_command(self, user):
+        with self.lock:
+            if self.clear_user_from_queue(user.id):
+                self.db.set_collection('queue', self.queue)
+                self.db.save()
+            yield TextMessage(f'{user.display_name} leaves queue for every Red Star')
             yield from self.generate_queue_text()
-
-    def start_command(self, user, level, spec):
-        key = self.build_key(level, spec)
-        if user.id not in self.queue[key]:
-            yield TextMessage(f'{user.display_name} not in queue for Red Star {level} {spec}')
-        else:
-            yield TextMessage(f'{user.display_name} starts queue for Red Star {level} {spec}')
-            yield from self.generate_start_text(level, spec)
-            for queue_uid in list(self.queue[key]):
-                self.clear_user_from_queue(queue_uid)
-            self.db.set_collection('queue', self.queue)
-            self.db.save()
-            yield from self.generate_queue_text()
-
-    def out_command_all(self, user):
-        if self.clear_user_from_queue(user.id):
-            self.db.set_collection('queue', self.queue)
-            self.db.save()
-        yield TextMessage(f'{user.display_name} leaves queue for every Red Star')
-        yield from self.generate_queue_text()
